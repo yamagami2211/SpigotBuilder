47a3a9f4e2d23dc51e8d4da34cbebdfd930f1482
yamagami2211 <mail@yama2211.jp>
2023-08-26 23:51:12 +0900

    Re #1 typo lastest

--- src/SpigotBuilder/Form1.Designer.cs
+++ src/SpigotBuilder/Form1.Designer.cs
@@ -67,7 +67,7 @@
             // 
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
-            "lastest"});
+            "latest"});
             this.comboBox1.Location = new System.Drawing.Point(14, 24);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 20);
